8c144cbf6f0ab880b02a5e4f7092a88fd722da3f
Honey-Joe <honeyjoe943@gmail.com>
2024-08-14 01:04:47 +0530

    update course

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Card_intro from '../Card_intro/Card_intro'
+import Course from '../Course/Course'
 
 const Body = () => {
   return (
     <>
-        <Card_intro></Card_intro> 
+        <Card_intro></Card_intro>
+        <Course></Course>
     </>
   )
 }
