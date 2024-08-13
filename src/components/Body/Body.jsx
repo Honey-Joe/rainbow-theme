363c4110e6f5425f421cef0645712fb46dc83417
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2024-08-14 01:29:17 +0530

    rainbow style two

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Card_intro from '../Card_intro/Card_intro'
+import StyleTwo from '../StyleTwo/StyleTwo'
 
 const Body = () => {
   return (
     <>
         <Card_intro></Card_intro> 
+        <StyleTwo/>
     </>
   )
 }
