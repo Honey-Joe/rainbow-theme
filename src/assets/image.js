87ac59c6b72f5c0c9193abfd09c114f1e77a9357
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2024-08-15 01:05:49 +0530

    completed

--- src/assets/image.js
+++ src/assets/image.js
@@ -3,4 +3,7 @@ export const rainbow_logo ="https://ik.imagekit.io/HoneyJoe/Rainbow%20theme/rain
 
 // style two img 
 export const img1 = "https://ik.imagekit.io/jjyo3gsee/Rainbow%20Themes/rainbow%20img/im1.webp?updatedAt=1723568534282";
-export const card_img= "https://ik.imagekit.io/HoneyJoe/Rainbow%20theme/rainbow%20theme/basics-of-histudy-theme.png?updatedAt=1723578078798"
+export const card_img= "https://ik.imagekit.io/HoneyJoe/Rainbow%20theme/rainbow%20theme/basics-of-histudy-theme.png?updatedAt=1723578078798";
+
+// background image 
+export const bgimg="https://ik.imagekit.io/jjyo3gsee/Rainbow%20Themes/rainbow%20img/tree-shape.svg?updatedAt=1723663771459";
--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -3,14 +3,16 @@ import React from "react"
 import {useTypewriter ,Cursor, Typewriter} from "react-simple-typewriter"
 import { LinearGradient } from "react-text-gradients"
 import Coursecard from "../Coursecard/Coursecard"
+import SubscribeBtn from "../SubscribeBtn/SubscribeBtn"
+import { bgimg } from "../../assets/image"
 
 
 const Course = () => {
      
   return (
     <>
-        <div className="2xl:container" id="corse">
-            <div className="w-[90%] xl:w-[65%]  mx-auto grid grid-cols-1 py-11">
+        <div className="2xl:container" >
+            <div className="w-[90%] xl:w-[65%]  mx-auto grid grid-cols-1 py-11 pb-44 bg-opacity-5" style={{backgroundImage:`url(${bgimg})`}} id="corse">
                 <div className="flex flex-col gap-3">
                     <div className="flex justify-center items-center">
                         <div className="shadow-2xl py-3 px-5 rounded-lg flex justify-center items-center">
@@ -24,7 +26,7 @@ const Course = () => {
                         ></Typewriter><Cursor></Cursor></LinearGradient></span></span>
                     </div>
                     <div className="flex justify-center items-center">
-                        <p className="text-center font-[euclid] text-[20px] text-[#6b7385]">Dive in and learn React.js from scratch! Learn Reactjs, Hooks, Redux, React Routing, Animations, Next.js and way more!</p>
+                        <p className="text-center font-[euclid] text-[20px] text-[#6b7385] md:px-[230px]">Dive in and learn React.js from scratch! Learn Reactjs, Hooks, Redux, React Routing, Animations, Next.js and way more!</p>
                     </div>
                     <div className="flex items-center justify-center gap-5">
                         <div>
